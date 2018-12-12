792656ff6d39ee48ba33c4dab0e0051f6b038d6a
ameba23 <ameba23@systemli.org>
2018-12-12 18:06:41 +0100

    generate keys

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const k = require('keythereum')
 const secp256k1 = require('secp256k1')
 const { randomBytes, createHash } = require('crypto')
-const sha256 = createHash('sha256');
+const sha256 = createHash('sha256')
 
-function generateKeys() {
+
+module.exports = function generateKeys () {
   let secretKey
   do {
     secretKey = randomBytes(32)
@@ -16,18 +17,23 @@ function generateKeys() {
   }
 }
 
-const alice = generateKeys()
-const bob = generateKeys()
-
-console.log('ecdh: ', secp256k1.ecdh(alice.publicKey, bob.secretKey))
-console.log('ecdh: ', secp256k1.ecdh(bob.publicKey, alice.secretKey))
-
-
-console.log('ecdhUnsafe with a sha256, to demonstrate it is the same as ecdh: ',sha256.update(secp256k1.ecdhUnsafe(alice.publicKey, bob.secretKey)).digest())
-
-console.log('ecdhUnsafe', secp256k1.ecdhUnsafe(alice.publicKey, bob.secretKey))
-console.log('ecdhUnsafe', secp256k1.ecdhUnsafe(bob.publicKey, alice.secretKey))
-
-
-
-console.log('Ethereum address: ', k.privateKeyToAddress(alice.secretKey))
+// const alice = generateKeys()
+// const bob = generateKeys()
+//
+// console.log('Alice secret key: ', alice.secretKey.toString('base64'))
+// console.log('Alice public key: ', alice.publicKey.toString('base64'))
+//
+// console.log('Bob secret key: ', bob.secretKey.toString('base64'))
+// console.log('Bob public key: ', bob.publicKey.toString('base64'))
+//
+// console.log('ecdh shared secret alicepk, bobsk ', secp256k1.ecdh(alice.publicKey, bob.secretKey).toString('base64'))
+// console.log('ecdh shared secret bobpk, alicesk ', secp256k1.ecdh(bob.publicKey, alice.secretKey).toString('base64'))
+//
+//
+// console.log('sha256(ecdhUnsafe), to demonstrate it is the same as ecdh: ', sha256.update(secp256k1.ecdhUnsafe(alice.publicKey, bob.secretKey)).digest().toString('base64'))
+//
+// console.log('ecdhUnsafe alicepk, bobsk: ', secp256k1.ecdhUnsafe(alice.publicKey, bob.secretKey).toString('base64'))
+// console.log('ecdhUnsafe bobsk, alicepk: ', secp256k1.ecdhUnsafe(bob.publicKey, alice.secretKey).toString('base64'))
+//
+// console.log('Alice\'s Ethereum address: ', k.privateKeyToAddress(alice.secretKey))
+// console.log('Bob\'s Ethereum address: ', k.privateKeyToAddress(bob.secretKey))
