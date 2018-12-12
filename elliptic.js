fa595f4184e795935199ce231cb6e85625fc161f
ameba23 <ameba23@systemli.org>
2018-12-12 18:06:23 +0100

    example of using elliptic

--- elliptic.js
+++ elliptic.js
@@ -1,13 +1,16 @@
-var EC = require('elliptic').ec;
-var ec = new EC('secp256k1');
-
+var EC = require('elliptic').ec
+var ec = new EC('secp256k1')
+const BN = require('bn.js')
 // Generate keys
-var key1 = ec.genKeyPair();
-var key2 = ec.genKeyPair();
+var key1 = ec.genKeyPair()
+var key2 = ec.genKeyPair()
 
-var shared1 = key1.derive(key2.getPublic());
-var shared2 = key2.derive(key1.getPublic());
+console.log(key1.priv.toBuffer().length)
+console.log(key1.getPublic())
 
-console.log('Both shared secrets are BN instances');
-console.log(shared1.toString(16));
-console.log(shared2.toString(16));
+var shared1 = key1.derive(key2.getPublic())
+var shared2 = key2.derive(key1.getPublic())
+
+console.log('Both shared secrets are BN instances')
+console.log(shared1.toString(16))
+console.log(shared2.toString(16))
