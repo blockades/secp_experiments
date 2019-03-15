4c27106082617477a85c76c1041f00f944c01e7d
ameba23 <ameba23@systemli.org>
2019-03-15 11:58:41 +0100

    add more info to demo

--- ssb-server.js
+++ ssb-server.js
@@ -1,5 +1,8 @@
-var CreateTestSbot = require('scuttle-testbot')
-var ssbKeys = require('ssb-keys')
+const CreateTestSbot = require('scuttle-testbot')
+const ssbKeys = require('ssb-keys')
+
+// A demonstration of publishing messages with both secp256k1 and ed25519
+// keypairs on the same instance of sbot
 
 var pietKeys = ssbKeys.generate('secp256k1')
 var katieKeys = ssbKeys.generate()
@@ -9,10 +12,13 @@ var myTempSbot = CreateTestSbot({name: 'testBotName', keys: pietKeys})
 var katie = myTempSbot.createFeed(katieKeys)
 var piet = myTempSbot.createFeed(pietKeys)
 
-piet.add({type: 'post', content: 'a message from piet, who signs with a secp256k1 keypair'}, (err, data) => {
+console.log("Piet's secp256k1 feed id:", piet.id)
+console.log("Katie's ed25519 feed id:", katie.id)
+
+piet.add({type: 'post', content: 'A message from piet, who signs with a secp256k1 keypair'}, (err, data) => {
   if (err) throw err
   console.log(data)
-  katie.add({type: 'post', content: 'a message from katie who signs with an ed25519 keypair'},function(err, data) {
+  katie.add({type: 'post', content: 'A message from katie who signs with an ed25519 keypair'}, (err, data) => {
     if (err) throw err
     console.log(data)
     myTempSbot.close()
